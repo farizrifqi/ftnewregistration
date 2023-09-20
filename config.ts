0099554915c25a6c439b6758129aeb92e680e9b7
zeraneL <farizrifqi26@gmail.com>
2023-09-20 20:18:55 +0800

    switch to typescript

---
+++ config.ts
@@ -0,0 +1,11 @@
+interface Config {
+    rpc: string;
+    friendTechAddress: string;
+}
+
+const config: Config = {
+    "rpc": "",
+    "friendTechAddress": "0xcf205808ed36593aa40a44f10c7f7c2f67d4a4d4"
+}
+
+export default config;
--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
-const fs = require('fs');
-const ethers = require('ethers');
-const config = require('./config.json');
-
+import { BlockTag, TransactionResponse, ethers } from "ethers";
+import config from "./config";
 const provider = (config.rpc.toLowerCase().includes("ws://") || config.rpc.toLowerCase().includes("wss://")) ? new ethers.WebSocketProvider(config.rpc, { name: "Base Mainnet", chainId: 8453 }) : new ethers.JsonRpcProvider(config.rpc);
 
-const isFirstBuy = (txData) => {
+const isFirstBuy = (txData: TransactionResponse) => {
     let { nonce, from, data, to } = txData
-    if (!data || !to) return;
+    if (!data || !to || !from) return;
 
     // the tx sould be it first tx
     let firstTx = nonce == 0;
     // check the tx buying a key from signer 
     let shareToFrom = data.toLowerCase().includes(from.substring(2).toLowerCase());
     // amount should be 1 key
-    let dataAmount = data.substring(data.length - 1) == 1;
+    let dataAmount = (data.substring(data.length - 1) == "1")
     // tx to friend.tech contract address
     let toFriendTech = to.toLowerCase() == config.friendTechAddress.toLowerCase();
 
@@ -24,14 +22,12 @@ const isFirstBuy = (txData) => {
 (async () => {
 
     // Listen to block update, since Base had no mempool can't listen the pending txs
-    provider.on("block", async (blockNumber) => {
-
+    provider.on("block", async (blockNumber: BlockTag) => {
         // Extracting data from block
         let blockData = await provider.getBlock(blockNumber, true);
 
         // Get transactions from the block
-        let blockTxs = blockData.prefetchedTransactions.filter(tx => (isFirstBuy(tx)));
-
+        let blockTxs = blockData?.prefetchedTransactions.filter((tx: TransactionResponse) => (isFirstBuy(tx))) || [];
         if (blockTxs.length < 1) return;
 
         for await (const tx of blockTxs) {
